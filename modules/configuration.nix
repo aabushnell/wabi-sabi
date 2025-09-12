fe9522c57568bb1ad8e094a936f16c36f9788e17
Aaron Bushnell <aabushnell@gmail.com>
2025-09-12 18:58:35 +0200

    move homebrew options to dedicated file

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -1,33 +1,5 @@
 { inputs, pkgs, config, ... }: {
 
-  homebrew = {
-    enable = true;
-    brews = [
-      "mas"
-    ];
-    casks = [
-      "blockblock"
-      "citrix-workspace"
-      "dhs"
-      "knockknock"
-      "lulu"
-      "microsoft-word"
-      "netiquette"
-      "oversight"
-      "private-internet-access"
-      "ransomwhere"
-      "reikey"
-      "stremio"
-      "taskexplorer"
-    ];
-    masApps = {
-      "Calendars" = 608834326;
-    };
-    taps = builtins.attrNames config.nix-homebrew.taps;
-    onActivation.cleanup = "zap";
-    onActivation.autoUpdate = true;
-    onActivation.upgrade = true;
-  };
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
---
+++ modules/homebrew.nix
@@ -0,0 +1,38 @@
+{ ... }: {
+
+  homebrew = {
+
+    enable = true;
+
+    brews = [
+      "mas"
+    ];
+
+    casks = [
+      "blockblock"
+      "citrix-workspace"
+      "dhs"
+      "knockknock"
+      "lulu"
+      "microsoft-word"
+      "netiquette"
+      "oversight"
+      "private-internet-access"
+      "ransomwhere"
+      "reikey"
+      "stremio"
+      "taskexplorer"
+    ];
+
+    masApps = {
+      "Calendars" = 608834326;
+    };
+
+    taps = builtins.attrNames config.nix-homebrew.taps;
+
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+  };
+
+}
