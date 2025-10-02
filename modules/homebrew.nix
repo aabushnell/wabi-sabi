e2ab24363d0a7815da9278214f208d64de016d36
Aaron Bushnell <aabushnell@gmail.com>
2025-10-02 20:47:43 +0200

    homebrew: add source comments

--- modules/homebrew.nix
+++ modules/homebrew.nix
@@ -9,19 +9,33 @@
     ];
 
     casks = [
+      # source: objective-see
       "blockblock"
+      # source: citrix systems
       "citrix-workspace"
+      # source: objective-see
       "dhs"
+      # source: objective-see
       "knockknock"
+      # source: objective-see
       "lulu"
+      # source: microsoft
       "microsoft-word"
+      # source: objective-see
       "netiquette"
+      # source: objective-see
       "oversight"
+      # source: private internet access
       "private-internet-access"
+      # source: objective-see
       "ransomwhere"
+      # source: objective-see
       "reikey"
+      # source: stremio
       "stremio"
+      # source: objective-see
       "taskexplorer"
+      # source: cisco systems
       "webex"
     ];
 
