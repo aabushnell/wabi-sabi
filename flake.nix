5eda0570f4e6ebf61da3cf996cc03b3c2d17e6c4
Aaron Bushnell <aabushnell@gmail.com>
2025-10-06 19:44:14 +0200

    remove miyoshi theme

--- flake.nix
+++ flake.nix
@@ -35,109 +35,89 @@
 
   };
 
-  outputs = inputs@{
-    self,
-    nixpkgs,
-    nix-darwin,
-    nix-homebrew,
-    homebrew-core,
-    homebrew-cask,
-    home-manager,
-    fenix,
-    themes,
-    ...
-  }: 
-  let
-    bahoukan-config = { pkgs, config, ... }: {
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , nix-darwin
+    , nix-homebrew
+    , homebrew-core
+    , homebrew-cask
+    , home-manager
+    , fenix
+    , themes
+    , ...
+    }:
+    let
+      bahoukan-config = { pkgs, config, ... }: {
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-        "pipe-operators"
-      ];
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = [
+          "nix-command"
+          "flakes"
+          "pipe-operators"
+        ];
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
 
-      networking.hostName = "miyoshi";
+        networking.hostName = "miyoshi";
 
-      users.users.aaron = {
-        name = "aaron";
-	      home = "/Users/aaron";
+        users.users.aaron = {
+          name = "aaron";
+          home = "/Users/aaron";
+        };
+
+        system.primaryUser = "aaron";
+
       };
 
-      system.primaryUser = "aaron";
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#hecate
+      darwinConfigurations."miyoshi" = nix-darwin.lib.darwinSystem {
+        specialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          bahoukan-config
 
-    };
+          home-manager.darwinModules.home-manager
 
-    miyoshiTheme = themes.custom {
-      name = "Miyoshi Gruvbox Theme";
-      author = "aaron";
-      base00 = "272727";
-      base01 = "CC231C";
-      base02 = "989719";
-      base03 = "D79920";
-      base04 = "448488";
-      base05 = "B16185";
-      base06 = "689D69";
-      base07 = "A89983";
-      base08 = "928373";
-      base09 = "FB4833";
-      base0A = "B8BA25";
-      base0B = "FABC2E";
-      base0C = "83A597";
-      base0D = "D3859A";
-      base0E = "8EC07B";
-      base0F = "EBDBB2";
-    };
+          ./modules/home-manager.nix
+          ./modules/packages.nix
+          ./modules/homebrew.nix
 
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#hecate
-    darwinConfigurations."miyoshi" = nix-darwin.lib.darwinSystem {
-      specialArgs = { 
-        miyoshiTheme = miyoshiTheme;
+          ./modules/shell.nix
+          ./modules/rust.nix
+
+          ./modules/fonts.nix
+
+          ./modules/system/dock.nix
+          ./modules/system/finder.nix
+          ./modules/system/sudo.nix
+
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "aaron";
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+              mutableTaps = false;
+            };
+          }
+        ];
       };
-      modules = [ 
-        bahoukan-config
-
-        home-manager.darwinModules.home-manager
-
-        ./modules/home-manager.nix
-        ./modules/packages.nix
-        ./modules/homebrew.nix
-
-        ./modules/shell.nix
-        ./modules/rust.nix
-
-        ./modules/fonts.nix
-
-        ./modules/system/dock.nix
-        ./modules/system/finder.nix
-        ./modules/system/sudo.nix
-
-	      nix-homebrew.darwinModules.nix-homebrew {
-	        nix-homebrew = {
-            enable = true;
-            enableRosetta = true;
-            user = "aaron";
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-	          };
-	          mutableTaps = false;
-	        };
-	      }
-      ];
     };
-  };
 } 
