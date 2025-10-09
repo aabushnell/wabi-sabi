620f1e1d91cf7f039b76a907731d14a9bdb15dd5
Aaron Bushnell <aabushnell@gmail.com>
2025-10-09 15:20:05 +0200

    [common;starship]: enable nu integration

--- home/common/starship.nix
+++ home/common/starship.nix
@@ -3,6 +3,7 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    enableNushellIntegration = true;
     settings = {
       add_newline = true;
       continuation_prompt = "[▸▹ ](dimmed white)";
