719bb220231d97940765b7afa30bbc44b33c2474
Aaron Bushnell <aabushnell@gmail.com>
2025-10-09 15:03:39 +0200

    [common;shell]: removed zsh-nix-shell plugin

--- home/common/shell.nix
+++ home/common/shell.nix
@@ -17,16 +17,6 @@
           sha256 = "eb5a5WMQi8arZRZDt4aX1IV+ik6Iee3OxNMCiMnjIx4=";
         };
       }
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
     ];
     oh-my-zsh = {
       enable = true;
