4cce78462b4b1dce90106089293a26f60ef7988e
Aaron Bushnell <aabushnell@gmail.com>
2025-12-08 20:25:44 +0100

    [kizaemon;default]: created default host file for kizaemon

--- hosts/kizaemon/default.nix
+++ hosts/kizaemon/default.nix
@@ -1,3 +1,22 @@
+{ pkgs, ... }:
+
 {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ]
+
+  # Machine Identity
+  networking.hostName = "kizaemon";
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # State Version
+  system.stateVersion = "25.05";
 
 }
