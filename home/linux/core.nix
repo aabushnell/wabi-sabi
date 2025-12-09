6cb5c29f94a611b3d2b2008c4da74071295157ab
Aaron Bushnell <aabushnell@gmail.com>
2025-12-09 13:28:35 +0100

    [home;linux]: add ssh config

--- home/linux/core.nix
+++ home/linux/core.nix
@@ -1,3 +1,32 @@
-{ username, ... }: {
+{ pkgs, username, ... }: 
+
+{
+
+  imports = [
+    # inputs.plasma-manager.homeManagerModules.plasma-manager
+  ];
+
   home.homeDirectory = "/home/${username}";
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+	user = "git";
+	identityFile = "~/.ssh/id_ed25519_GITHUB";
+	extraOptions = {
+	  AddKeysToAgent = "yes";
+	};
+      };
+    };
+  };
+
+  services.ssh-agent.enable = true;
+
+  home.sessionVariables = {
+    SSH_ASKPASS = "${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
+    SSH_ASKPASS_REQUIRE = "prefer";
+  };
+
 }
